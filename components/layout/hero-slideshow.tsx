678146860dc757d5825957c940830309764f9099
JoskcyX <pekajoshy@gmail.com>
2026-09-09 18:32:38 +0100

    new changes

--- components/layout/hero-slideshow.tsx
+++ components/layout/hero-slideshow.tsx
@@ -28,6 +28,7 @@ export function HeroSlideshow({ photos }: { photos: string[] }) {
               alt=""
               fill
               priority={i === 0}
+              quality={90}
               sizes="100vw"
               className="object-cover"
             />
